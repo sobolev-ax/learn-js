c2afb2b9cc79973b177e117ac5c9da861f03dc36
sobolev-ax <sobolev.ax@gmail.com>
2017-07-11 18:32:12 +0300

    end chapter /string

--- js/2.2.2.js
+++ js/2.2.2.js
@@ -1,4 +1,210 @@
 "use strict";
+
+// // Есть стоимость в виде строки: "$120". То есть, первым идёт знак валюты, а затем – число.
+// // Создайте функцию extractCurrencyValue(str), которая будет из такой строки выделять
+// // число-значение, в данном случае 120.
+// function extractCurrencyValue(str) {
+//     return +str.slice(1);
+// }
+// console.log(extractCurrencyValue("$120"));
+// console.log(extractCurrencyValue("$1300"));
+// console.log(extractCurrencyValue("Р120"));
+
+// // Создайте функцию truncate(str, maxlength), которая проверяет длину
+// // строки str, и если она превосходит maxlength – заменяет конец str
+// // на "...", так чтобы ее длина стала равна maxlength.
+// function truncate(str, maxlength) {
+//     if (str.length <= maxlength) { // если подходит то сразу возращаем строку
+//         return str;
+//     } else {
+//         return str.slice(0, maxlength-3) + "...";
+//     }
+// }
+// console.log(
+//     truncate("Вот, что мне хотелось бы сказать на эту тему:", 20) == "Вот, что мне хоте...");
+// console.log(
+//     truncate("Всем привет!", 20) == "Всем привет!");
+
+// //Напишите функцию checkSpam(str), которая возвращает true, если строка str содержит „viagra“ или „XXX“, а иначе false
+// function checkSpam(str) {
+//     return ~str.toLowerCase().indexOf("xxx") || ~str.toLowerCase().indexOf("viagra") ? true : false;
+// }
+// console.log(checkSpam('buy ViAgRA now') == true);
+// console.log(checkSpam('free xxxxx') == true);
+// console.log(checkSpam("innocent rabbit") == false);
+
+// //Напишите функцию ucFirst(str), которая возвращает строку str с заглавным первым символом
+// function ucFirst(str) {
+//     var firstChar = str.charAt(0);
+//     firstChar = firstChar.toUpperCase();
+//     str = str.slice(1);
+//     //console.log(firstChar + str);
+//     return firstChar + str;
+// }
+// console.log(ucFirst("вася") == "Вася");
+// console.log(ucFirst("") == ""); // нет ошибок при пустой строке
+
+// // Поиск всех вхождений
+// function f() {
+//     var str = "23 23 2 getdrgdf trhrthr getOOgetgetget"; //4 -совпадения
+//     var index = 0;
+//     while (1) {
+//         index = str.indexOf("get", index);
+//         if (index === -1) return 1;
+//         console.log(index);
+//         index++;
+//     }
+// };
+// f();
+
+// var str = "Widget";
+// if(~str.indexOf("get")) {
+//     alert ('Совпадение');
+// } else {
+//     alert (':(');
+// }
+
+// console.log("".charAt(0));
+// console.log(""[0]);
+
+// //Напишите код для генерации случайного значения в диапазоне от 0 до max, не включая max.
+// function random(max) {
+//     alert( Math.random() * max );
+// }
+// console.log(random(300));
+
+// function fib(n) {
+//     var a = 1,
+//         b = 0,
+//         x;
+//     for (var i = 0; i < n; i++) {
+//         x = a + b;
+//         a = b;
+//         b = x;
+//     }
+//     return b;
+// }
+// function fibBinet(n) {
+//     var fi = (1 + Math.sqrt(5)) / 2;
+//     return Math.round( Math.pow(fi, n) / Math.sqrt(5) )
+// }
+// console.log(fibBinet(77));
+// console.log(fib(77));
+
+// function getDecimal(n) {
+//     if (n < 0) n = -n;
+//     return Math.round((n - parseInt(n)) * 1000) / 1000;
+// }
+// alert( getDecimal(12.345) ); // 0.345
+// alert( getDecimal(1.2) ); // 0.2
+// alert( getDecimal(-1.2) ); // 0.2
+// // function getDecimal(num) { // работаем как со строкой
+// //     var str = "" + num;
+// //     var zeroPos = str.indexOf(".");
+// //     if (zeroPos == -1) return 0;
+// //     str = str.slice(zeroPos);
+// //     return +str;
+// // }
+
+// var i = 0;
+// while (i < 11) {
+//     i += 0.2;
+//     if (i > 9.8 && i < 10.2) alert( i );
+// }
+
+// // alert( 0.1 + 0.2 + '$' );
+// // Что можно сделать, чтобы избежать проблем с ошибками округления?
+// alert( (0.1 + 0.2).toFixed(1) + '$');
+
+// console.log( 6.35.toFixed(20));
+
+// //Создайте страницу, которая предлагает ввести два числа и выводит их сумму.
+// var numberOne = +prompt("Число первое:");
+// var numberTwo = +prompt("Число второе:");
+// alert(
+//     numberOne + numberTwo
+// );
+
+// var n = 10000000;
+// console.log( n.toLocaleString() );
+// n = 10000;
+// console.log( n.toLocaleString() );
+
+// console.log( 0.1 + 0.2 /*=== 0.3*/);
+// console.log(9999999999999999);
+
+// var n = 3.456;
+// console.log( Math.round(n * 100) / 100 ); // Более точный и правильный метод
+// console.log( +n.toFixed(2) );
+
+// console.log( Math.floor(3.1));
+// console.log( Math.ceil(3.1));
+// console.log( Math.round(3.1));
+// console.log( ~~3.1);
+// console.log( 1.2 + 1.3 ^ 0);
+
+// var n = 255;
+// console.log( n.toString(16) );
+
+
+// function isNumeric(n) {
+//     return !isNaN(parseFloat(n) && isFinite(n));
+// };
+
+// var x = prompt("Введите значение", "-11.4");
+// if (isNaN(x)) {
+//     alert( "Строка преобразовалась в NaN. Не число." );
+// } else {
+//     alert( "Число" );
+// }
+
+// console.log( parseInt("12px") );
+// console.log( parseInt("12.456px") );
+// console.log( parseFloat("12px") );
+// console.log( parseFloat("12.456px") );
+// console.log( parseInt("FF", 16) );
+
+// console.log(+"nfng");
+// console.log(" - NaN");
+// console.log(+"2323");
+// console.log(" - число");
+// console.log("2323");
+// console.log(" - строка");
+
+// //Функция isFinite(n) преобразует аргумент к числу и возвращает true, если это не NaN/Infinity/-Infinity
+// console.log( isFinite(1) );
+// console.log( isFinite(Infinity) );
+// console.log( isFinite(NaN));
+
+// var value = prompt("Введити Infinity", 'Infinity');
+// var number = +value;
+// console.log( number );
+
+// console.log(0xff);
+// console.log(2e3);
+// console.log(33e-2);
+// console.log(1/0);
+// console.log(-1/0);
+// console.log(1e500);
+// console.log( 0 / 0); // NaN ничему не равно
+// console.log( isNaN(2) );
+// console.log( isNaN("") );
+// console.log( isNaN(0) );
+// console.log( isNaN(Infinity) );
+// console.log( isNaN("123") );
+// console.log( isNaN("sdfdf") );
+
+// var number = 3.14566;
+// console.log(number.toFixed(2));
+// console.log(number.toFixed(0));
+// console.log(number.toFixed(10));
+// console.log(12..toFixed(3));
+
+// var helloWorldStrict = "Hello World!";
+// console.log(helloWorldStrict.length);
+// console.log(helloWorldStrict.toUpperCase());
+// console.log(helloWorldStrict.toUpperCase);
+
 function pow(number, elevate) {
     if (elevate === 0 && number === 0) {
         return undefined;
