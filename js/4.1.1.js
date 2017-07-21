b33105fdb0abd16f6b499195fdb2946b43e6c7c6
sobolev-ax <sobolev.ax@gmail.com>
2017-07-21 12:02:42 +0300

    start chapter /modifying-document

--- js/4.1.1.js
+++ js/4.1.1.js
@@ -1,1 +1,9 @@
-console.log("HI");
+for (var i = 0; i < document.body.children.length; i++) {
+    console.log( document.body.children[i]);
+}
+
+// for (var i = 0; i < document.body.childNodes.length; i++) {
+//     console.log( document.body.childNodes[i]);
+// }
+
+//alert( document.body );
